b748ebb1c26e5475e5a8118a57df91467c88865d
Kokutse DJOGUENOU <kokutse@Kokutses-MacBook-Pro.local>
2022-04-25 09:36:10 -0400

    Adding state management

--- src/components/Mole.jsx
+++ src/components/Mole.jsx
@@ -2,7 +2,14 @@ import { useEffect } from 'react'
 import moleImage from '../mole.png'
 
 const Mole = (props) => {
-
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 10000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+    
     return (
         <div>
             <img  src={moleImage} onClick={props.handleClick} />
